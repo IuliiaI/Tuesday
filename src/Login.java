f0b06adfe3ffbc3146a3f65d94eca30c4a1dd404
Khan Afsar <camrose2022@hotmail.com>
2022-12-20 10:41:25 -0800

    issue resolved

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,6 @@
 public class Login {
     public static void main(String[] args) {
         System.out.println("I am working on Login features");
-        String nam=123;
+        String nam="123";
     }
 }
