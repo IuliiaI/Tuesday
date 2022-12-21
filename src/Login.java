c19987de684f9fda5a7a7a162e6d715f95813a02
Khan Afsar <camrose2022@hotmail.com>
2022-12-21 11:01:36 -0800

    code issue

--- src/Login.java
+++ src/Login.java
@@ -2,5 +2,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("I am working on Login features");
         String nam="123";
+        int age="String";
     }
 }
